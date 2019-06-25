f1b7ec18eb3c572eb4686c722861b6746639f634
anirudhvyas88 <anirudhvyas88@gmail.com>
2019-06-25 08:41:29 -0400

    Edited Hello world message

--- src/main/java/sampleproject11/HelloDevOpsWorld.java
+++ src/main/java/sampleproject11/HelloDevOpsWorld.java
@@ -4,7 +4,7 @@ public class HelloDevOpsWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hellow DevOps World once again!!!");
+		System.out.println("Hellow DevOps World!!!");
 		
 	}
 
