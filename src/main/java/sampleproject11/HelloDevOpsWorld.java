dc4944552c07e4d00ca7dee35c4d6aac698f3ff2
anirudhvyas47 <anirudhvyas88@gmail.com>
2019-06-26 13:53:15 -0400

    changes hello world message

--- src/main/java/sampleproject11/HelloDevOpsWorld.java
+++ src/main/java/sampleproject11/HelloDevOpsWorld.java
@@ -4,7 +4,7 @@ public class HelloDevOpsWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello DevOps World!!!");
+		System.out.println("Hello World again!!!");
 		
 	}
 
